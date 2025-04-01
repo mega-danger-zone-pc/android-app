8f3e26dbf98dcaa68e38a85cde9339ca07c7d2ab
Mihail <mihail@mail.com>
2025-04-01 12:26:24 +0600

    дизайн делается

--- app/src/main/java/com/example/mega_app/LoginActivity.java
+++ app/src/main/java/com/example/mega_app/LoginActivity.java
@@ -1,2 +1,14 @@
-package com.example.mega_app;public class LoginActivity {
+package com.example.mega_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
